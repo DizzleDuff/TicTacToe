6b264e26fe7a6375f5468507ff360bed74d5f46d
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2019-10-08 17:17:45 -0400

    Organize text lines

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,24 +1,26 @@
 /**
  * TicTacToe class implements the interface
+ * 
  * @author relkharboutly
  * @date 1/5/2017
  */
 public class TicTacToe implements ITicTacToe {
-		 
-	   // The game board and the game status
-	   private static final int ROWS = 3, COLS = 3; // number of rows and columns
-	   private int[] board = new int[ROWS*COLS]; // game board in 2D array
-	  
+
+	// The game board and the game status
+	private static final int ROWS = 3, COLS = 3; // number of rows and columns
+	private int[] board = new int[ROWS * COLS]; // game board in 2D array
+
 	/**
-	 * clear board and set current player   
+	 * clear board and set current player
 	 */
-	public TicTacToe(){
-		
+	public TicTacToe() {
+
 	}
+
 	@Override
 	public void clearBoard() {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < ROWS*COLS; i++) {
+		for (int i = 0; i < ROWS * COLS; i++) {
 			board[i] = EMPTY;
 		}
 	}
@@ -32,7 +34,7 @@ public class TicTacToe implements ITicTacToe {
 	@Override
 	public int getComputerMove() {
 		// TODO Auto-generated method stub
-		if(board[0] == 0)
+		if (board[0] == 0)
 			return 0;
 		else if (board[1] == 0)
 			return 1;
@@ -48,72 +50,80 @@ public class TicTacToe implements ITicTacToe {
 			return 6;
 		else if (board[7] == 0)
 			return 7;
-		else 
+		else
 			return 8;
 	}
+
 	@Override
 	public int checkForWinner() {
 		if (checkWinner(CROSS))
 			return CROSS_WON;
-		else if (checkWinner(NOUGHT)) 
+		else if (checkWinner(NOUGHT))
 			return NOUGHT_WON;
-		else if (board[0] != EMPTY && board[1] != EMPTY && board[2] != EMPTY && board[3] != EMPTY && board[4] != EMPTY && board[5] != EMPTY &&
-				board[6] != EMPTY && board[7] != EMPTY && board[8] != EMPTY)
+		else if (board[0] != EMPTY && board[1] != EMPTY && board[2] != EMPTY && board[3] != EMPTY && board[4] != EMPTY
+				&& board[5] != EMPTY && board[6] != EMPTY && board[7] != EMPTY && board[8] != EMPTY)
 			return TIE;
-		else 
+		else
 			return PLAYING;
-			
-		
+
 	}
-	
+
 	public boolean isEmpty(int index) {
 		return board[index] == EMPTY;
 	}
-	  /**
-	   *  Print the game board 
-	   */
-	   public  void printBoard() {
-	      for (int row = 0; row < ROWS; ++row) {
-	         for (int col = 0; col < COLS; ++col) {
-	            printCell(board[col+row*3]); // print each of the cells
-	            if (col != COLS - 1) {
-	               System.out.print("|");   // print vertical partition
-	            }
-	         }
-	         System.out.println();
-	         if (row != ROWS - 1) {
-	            System.out.println("-----------"); // print horizontal partition
-	         }
-	      }
-	      System.out.println();
-	   }
-	 
-	   /**
-	    * Print a cell with the specified "content" 
-	    * @param content either CROSS, NOUGHT or EMPTY
-	    */
-	   public  void printCell(int content) {
-	      switch (content) {
-	         case EMPTY:  System.out.print("   "); break;
-	         case NOUGHT: System.out.print(" O "); break;
-	         case CROSS:  System.out.print(" X "); break;
-	      }
-	   }
+
+	/**
+	 * Print the game board
+	 */
+	public void printBoard() {
+		for (int row = 0; row < ROWS; ++row) {
+			for (int col = 0; col < COLS; ++col) {
+				printCell(board[col + row * 3]); // print each of the cells
+				if (col != COLS - 1) {
+					System.out.print("|"); // print vertical partition
+				}
+			}
+			System.out.println();
+			if (row != ROWS - 1) {
+				System.out.println("-----------"); // print horizontal partition
+			}
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Print a cell with the specified "content"
+	 * 
+	 * @param content either CROSS, NOUGHT or EMPTY
+	 */
+	public void printCell(int content) {
+		switch (content) {
+		case EMPTY:
+			System.out.print("   ");
+			break;
+		case NOUGHT:
+			System.out.print(" O ");
+			break;
+		case CROSS:
+			System.out.print(" X ");
+			break;
+		}
+	}
 
 	public boolean checkWinner(int player) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-				if (board[0] == player && board[1] == player && board[2] == player ||
-					board[3] == player && board[4] == player && board[5] == player ||
-					board[6] == player && board[7] == player && board[8] == player ||
-					board[0] == player && board[3] == player && board[6] == player ||
-					board[1] == player && board[4] == player && board[7] == player ||
-					board[2] == player && board[5] == player && board[8] == player ||
-					board[0] == player && board[4] == player && board[8] == player ||
-					board[2] == player && board[4] == player && board[6] == player) {
-				return true;	
-				} else
-					return false;
+		if (board[0] == player && board[1] == player && board[2] == player
+				|| board[3] == player && board[4] == player && board[5] == player
+				|| board[6] == player && board[7] == player && board[8] == player
+				|| board[0] == player && board[3] == player && board[6] == player
+				|| board[1] == player && board[4] == player && board[7] == player
+				|| board[2] == player && board[5] == player && board[8] == player
+				|| board[0] == player && board[4] == player && board[8] == player
+				|| board[2] == player && board[4] == player && board[6] == player) {
+			return true;
+		} else
+			return false;
 	}
 
 }
